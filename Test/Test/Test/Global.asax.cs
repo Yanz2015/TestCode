310bbc40a07c00c32feaa0c896d72387e4a83f94
Yan Zhao <yazha@microsoft.com>
2016-12-28 18:35:09 +0800

    Commit Test Files

---
+++ Test/Test/Test/Global.asax.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Test
+{
+    using System.Data.SqlClient;
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            using (var sqlconn = new SqlConnection("Data Source=g1sa681vaf.database.chinacloudapi.cn,1433;Initial Catalog=sampledb1;Persist Security Info=True;User ID=micdev;Password=mic@2016;MultipleActiveResultSets=True;Encrypt=True"))
+            {
+                if (sqlconn.State != System.Data.ConnectionState.Open)
+                {
+                    sqlconn.Open();
+                }
+            }
+        }
+    }
+}
